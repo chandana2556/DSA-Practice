dd2e96c4089de14bd2dbbd879bdd979b584fd4ee
chandana <madugulachandana2625@gmail.com>
2025-09-14 11:52:25 +0530

    Added Leetcode 27 Remove Element Solution in TwoPointers

---
+++ Array/TwoPointers/Remove_Element.cpp
@@ -0,0 +1,20 @@
+/* Remove Element 
+   Leetcode: https://leetcode.com/problems/remove-element/
+    
+   Time Complexity: O(n)
+   Space Complexity: O(1)
+*/
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int j=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=val){
+                nums[j]=nums[i];
+                j++;
+            }
+        }
+        return j;
+    }
+};
