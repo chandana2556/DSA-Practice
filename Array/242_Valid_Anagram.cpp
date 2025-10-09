c5effbae63b8e3038ae4828f1ff3958ba043d6c6
chandana <madugulachandana2625@gmail.com>
2025-10-09 20:16:13 +0530

    Added Leetcode folder inside Linked_List

---
+++ Array/242_Valid_Anagram.cpp
@@ -0,0 +1,14 @@
+//242 Valid Anagram
+
+//Brute Force Approach : O(nlogn)
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length()!=t.length())
+            return false;
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        return s==t;
+    }
+};
